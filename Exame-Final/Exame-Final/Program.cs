5572531915ad09ba410e8d6c38aafa7990689529
goncalocamisao <goncalo.camisao@my.istec.pt>
2023-02-28 21:32:01 +0000

    Gonçalo Camisão

--- Exame-Final/Exame-Final/Program.cs
+++ Exame-Final/Exame-Final/Program.cs
@@ -10,6 +10,42 @@ namespace Exame_Final
     {
         static void Main(string[] args)
         {
+            
+            Console.WriteLine("Introduzir os valores dos coeficientes a, b e c da equação do segundo grau:");
+            double a = double.Parse(Console.ReadLine());
+            double b = double.Parse(Console.ReadLine());
+            double c = double.Parse(Console.ReadLine());
+
+            
+            double delta = b * b - 4 * a * c;
+
+            
+            if (delta > 0)
+            {
+                
+                double x1 = (-b + Math.Sqrt(delta)) / (2 * a);
+                double x2 = (-b - Math.Sqrt(delta)) / (2 * a);
+
+                
+                Console.WriteLine("A equação tem duas raízes reais distintas:");
+                Console.WriteLine("x1 = " + x1);
+                Console.WriteLine("x2 = " + x2);
+            }
+            else if (delta == 0)
+            {
+                
+                double x = -b / (2 * a);
+
+                
+                Console.WriteLine("A equação tem uma raiz real única:");
+                Console.WriteLine("x = " + x);
+            }
+            else
+            {
+                
+                Console.WriteLine("A equação não tem solução real.");
+            }
         }
     }
 }
+
